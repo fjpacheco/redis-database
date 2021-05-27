8314d8c73733bc377e787c7c094937c19c54b1cb
Martina Ágata <39577698+martinaAgata@users.noreply.github.com>
2021-05-27 00:37:47 -0300

    Update decrby

--- src/commands/decrby.rs
+++ src/commands/decrby.rs
@@ -1,5 +1,5 @@
-use super::database::{execute_value_modification, Database};
-use crate::native_types::error::ErrorStruct;
+use crate::native_types::{error::ErrorStruct, integer::RInteger, redis_type::RedisType};
+use std::collections::HashMap;
 
 pub struct Decrby;
 
