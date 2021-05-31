33b302c3b728f2f0a49353d238fcac961919edb3
martinaagata <mpanetta@fi.uba.ar>
2021-05-31 20:33:41 -0300

    #42 lrange finished, #44 lset completed too

--- src/commands/lists/lrange.rs
+++ src/commands/lists/lrange.rs
@@ -41,20 +41,23 @@ pub fn find_elements_in_range(
     let len = values_list.len() as isize;
 
     if start < 0 {
-        start += len;
+        start += len; // start = 2
     }
 
     if stop < 0 {
-        stop += len;
+        stop += len; // stop = 2
     }
 
-    if start >= len || start > stop {
-        // lrange keyy 0 -1 is actually valid, but baby steps ok?
+    if start >= len || start > stop || stop < -len {
         Ok(RSimpleString::encode("(empty list or set)".to_string()))
-    } else if start >= 0 && stop >= 0 {
-        get_list_elements_in_range(start, stop, values_list)
     } else {
-        Ok(RSimpleString::encode("(empty list or set)".to_string()))
+        if start < 0 {
+            start = 0;
+        }
+        if stop >= len {
+            stop = len - 1; // check if -1 is needed
+        }
+        get_list_elements_in_range(start, stop, values_list)
     }
 }
 
@@ -73,12 +76,13 @@ pub fn get_list_elements_in_range(
 
     let mut range_elems: Vec<String> = vec![];
     let mut i = start;
-
+    let mut j = 1;
     while i < stop + 1 && iter_elem != None {
-        let elem = format!("{}) \"{}\"", i + 1, &iter_elem.unwrap().to_string());
+        let elem = format!("{}) \"{}\"", j, &iter_elem.unwrap().to_string());
         println!("{}", elem);
         range_elems.push(elem);
         i += 1;
+        j += 1;
         iter_elem = iter.next()
     }
     Ok(RArray::encode(range_elems))
@@ -180,7 +184,7 @@ pub mod test_lrange {
     }
 
     #[test]
-    fn test06_lrange_existing_list_with_many_elements_positive_range() {
+    fn test06_lrange_list_with_many_elements_positive_range() {
         let mut data = Database::new();
 
         let mut new_list = LinkedList::new();
@@ -200,7 +204,7 @@ pub mod test_lrange {
     }
 
     #[test]
-    fn test07_lrange_existing_list_with_many_elements_from_negative_first_index_to_zero() {
+    fn test07_lrange_list_with_many_elements_from_negative_first_index_to_zero() {
         let mut data = Database::new();
 
         let mut new_list = LinkedList::new();
@@ -219,7 +223,7 @@ pub mod test_lrange {
     }
 
     #[test]
-    fn test08_lrange_existing_list_with_many_elements_from_zero_to_negative_last_index() {
+    fn test08_lrange_list_with_many_elements_from_zero_to_negative_last_index() {
         let mut data = Database::new();
 
         let mut new_list = LinkedList::new();
@@ -239,7 +243,7 @@ pub mod test_lrange {
     }
 
     #[test]
-    fn test09_lrange_existing_list_with_many_elements_from_negative_out_of_range_number_to_valid_negative_index(
+    fn test09_lrange_list_with_many_elements_from_negative_out_of_range_number_to_valid_negative_index(
     ) {
         let mut data = Database::new();
 
@@ -251,12 +255,110 @@ pub mod test_lrange {
         data.insert("key".to_string(), TypeSaved::List(new_list));
 
         let buffer = vec!["key", "-20", "-2"];
-        let _encoded = Lrange::run(buffer, &mut data);
+        let encoded = Lrange::run(buffer, &mut data);
         // >lrange keyy -23 -2
-        // NOT IMPLEMENTED
         assert_eq!(
-            "*2\r\n$11\r\n1) \"value1\"\r\n$11\r\n2) \"value2\"\r\n".to_string(),
+            encoded.unwrap(),
             "*2\r\n$11\r\n1) \"value1\"\r\n$11\r\n2) \"value2\"\r\n".to_string()
+        );
+    }
+
+    #[test]
+    fn test10_lrange_list_with_many_elements_from_negative_out_of_range_number_to_invalid_negative_index(
+    ) {
+        let mut data = Database::new();
+
+        let mut new_list = LinkedList::new();
+        new_list.push_back("value1".to_string());
+        new_list.push_back("value2".to_string());
+        new_list.push_back("value3".to_string());
+
+        data.insert("key".to_string(), TypeSaved::List(new_list));
+
+        let buffer = vec!["key", "-20", "-10"];
+        // >lrange keyy -20 -10
+        let encoded = Lrange::run(buffer, &mut data);
+        assert_eq!(encoded.unwrap(), "+(empty list or set)\r\n".to_string());
+    }
+
+    #[test]
+    fn test11_lrange_list_with_many_elements_from_negative_out_of_range_number_to_number_bigger_than_len(
+    ) {
+        let mut data = Database::new();
+
+        let mut new_list = LinkedList::new();
+        new_list.push_back("value1".to_string());
+        new_list.push_back("value2".to_string());
+        new_list.push_back("value3".to_string());
+
+        data.insert("key".to_string(), TypeSaved::List(new_list));
+
+        let buffer = vec!["key", "-20", "20"];
+        let encoded = Lrange::run(buffer, &mut data);
+        // >lrange keyy -20 20
+        assert_eq!(
+            encoded.unwrap(),
+            "*3\r\n$11\r\n1) \"value1\"\r\n$11\r\n2) \"value2\"\r\n$11\r\n3) \"value3\"\r\n"
+                .to_string()
+        );
+    }
+
+    #[test]
+    fn test12_lrange_list_with_many_elements_from_negative_out_of_range_number_to_list_bottom() {
+        let mut data = Database::new();
+
+        let mut new_list = LinkedList::new();
+        new_list.push_back("value1".to_string());
+        new_list.push_back("value2".to_string());
+        new_list.push_back("value3".to_string());
+
+        data.insert("key".to_string(), TypeSaved::List(new_list));
+
+        let buffer = vec!["key", "-20", "-1"];
+        let encoded = Lrange::run(buffer, &mut data);
+        // >lrange keyy -20 -1
+        assert_eq!(
+            encoded.unwrap(),
+            "*3\r\n$11\r\n1) \"value1\"\r\n$11\r\n2) \"value2\"\r\n$11\r\n3) \"value3\"\r\n"
+                .to_string()
+        );
+    }
+
+    #[test]
+    fn test13_lrange_list_many_element_negative_indexing() {
+        let mut data = Database::new();
+
+        let mut new_list = LinkedList::new();
+        new_list.push_back("value1".to_string());
+        new_list.push_back("value2".to_string());
+        new_list.push_back("value3".to_string());
+
+        data.insert("key".to_string(), TypeSaved::List(new_list));
+
+        let buffer = vec!["key", "-1", "-1"];
+        let encoded = Lrange::run(buffer, &mut data);
+        assert_eq!(
+            encoded.unwrap(),
+            "*1\r\n$11\r\n1) \"value3\"\r\n".to_string()
+        );
+    }
+
+    #[test]
+    fn test14_lrange_list_many_element_from_negative_index_to_zero() {
+        let mut data = Database::new();
+
+        let mut new_list = LinkedList::new();
+        new_list.push_back("value1".to_string());
+        new_list.push_back("value2".to_string());
+        new_list.push_back("value3".to_string());
+
+        data.insert("key".to_string(), TypeSaved::List(new_list));
+
+        let buffer = vec!["key", "-3", "0"];
+        let encoded = Lrange::run(buffer, &mut data);
+        assert_eq!(
+            encoded.unwrap(),
+            "*1\r\n$11\r\n1) \"value1\"\r\n".to_string()
         );
     }
 }
