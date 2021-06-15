c831f1a76e6191a47c62d77fe11c02624c0af00a
Federico Pacheco <fpacheco@fi.uba.ar>
2021-06-15 08:18:17 -0300

    Test with Redis Client fixed

--- tests/test_crate.rs
+++ tests/test_crate.rs
@@ -1,18 +1,58 @@
 #[cfg(test)]
 mod testings_redis {
     extern crate redis;
-    use std::thread;
+    use std::thread::{self, JoinHandle};
 
     use redis::RedisError;
-    use redis_rust::{redis_config::RedisConfig, tcp_protocol::server::ServerRedis};
+    use redis_rust::{native_types::ErrorStruct, redis_config::RedisConfig, tcp_protocol::server::ServerRedis};
+
 
     #[test]
-    fn test01_crate() {
-        let _server_handler = thread::spawn(|| ServerRedis::start(vec![]));
+    fn test01_set_and_get() -> Result<(), ErrorStruct>{
+        let _server_thread: JoinHandle<Result<(), ErrorStruct>> = thread::spawn(|| {
+            ServerRedis::start(vec![])?;
+            Ok(())
+        });
 
         let client = redis::Client::open(
             "redis://".to_owned()
                 + &RedisConfig::default().ip()
+                + ":"
+                + &RedisConfig::default().port()
+                + "/",
+        )
+        .unwrap();
+
+        let mut conection_client = client.get_connection().unwrap();
+
+        let received: Result<String, RedisError> = redis::cmd("set")
+            .arg("key")
+            .arg("value")
+            .query(&mut conection_client);
+
+        assert_eq!(received.unwrap(), "OK");
+
+
+        let received: Result<String, RedisError> = redis::cmd("get")
+            .arg("key")
+            .query(&mut conection_client);
+        
+        assert_eq!(received.unwrap(), "value");
+
+        Ok(())
+    }
+
+    
+    #[test]
+    #[ignore]
+    fn test_random() {
+        let server_thread = thread::spawn(|| {
+            ServerRedis::start(vec![]);
+        });
+
+        let client = redis::Client::open(
+            "redis://".to_owned()
+            + &RedisConfig::default().ip()
                 + ":"
                 + &RedisConfig::default().port()
                 + "/",
@@ -41,5 +81,7 @@ mod testings_redis {
             .arg("Agustín")
             .query(&mut conection_client);
         println!("get Agustín => {:?}", received_4);
+
+
     }
 }
